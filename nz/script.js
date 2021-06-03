948ee23eb8eaaec78d67a23d858e09c85793ee9b
Maria Duvanova <d_muriam@inbox.ru>
2021-06-03 18:16:21 +0700

    new edit

--- nz/script.js
+++ nz/script.js
@@ -1,6 +1,12 @@
 
-function changeText(newText)
+function changeText()
 {
     var elem = document.getElementById("content");
-    elem.innerText = newText;
+    elem.innerHTML = 'Два крупных острова – Северный и Южный, образующие Новую Зеландию, и несколько десятков мелких островов у их побережья входят в состав одной из протягивающихся вдоль западной окраины Тихого океана островных дуг.';
+}
+
+function changeText1()
+{
+    var elem = document.getElementById("content");
+    elem.innerHTML = "Изначально маори проживали на территории северных островов.";
 }
