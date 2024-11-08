011453dba50eec67197044f4daa12b920c1ad9c5
Maria Duvanova <d_muriam@inbox.ru>
2024-11-08 22:55:30 +0700

    add checkboxes

--- Project/Hotels/urls.py
+++ Project/Hotels/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path('', views.func),
+    path('', views.func_get, name='func_get'), # GET запрос
+    path('submit/', views.func_post, name='func_post'), # POST запрос (куда будет отправляться форма)
 ]
