b509c236b66f929810ea3fe118f06e8d1f3be3ca
Maria Duvanova <d_muriam@inbox.ru>
2024-12-03 16:11:34 +0700

    remove folder django

--- django/Project/Hotels/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.func_get, name='func_get'), # GET запрос
-    path('submit/', views.func_post, name='func_post'), # POST запрос (куда будет отправляться форма)
-]
